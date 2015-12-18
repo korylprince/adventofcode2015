0f421e64789fc4d1e781e984ccecbac3234f93f0
Kory Prince <korylprince@gmail.com>
2015-12-18 14:11:55 -0600

    day 1 answer 2

--- 01/main.go
+++ 01/main.go
@@ -11,6 +11,23 @@ func FindLevel(in string) int {
 	return strings.Count(in, "(") - strings.Count(in, ")")
 }
 
+func FindLevelIndex(in string, level int) int {
+	var cur int
+	for idx, val := range in {
+		if val == '(' {
+			cur += 1
+		} else if val == ')' {
+			cur -= 1
+		} else {
+			panic(fmt.Errorf("Unexpected '%s'", val))
+		}
+		if cur == level {
+			return idx + 1 //starts at 1
+		}
+	}
+	panic(fmt.Errorf("Reached EOF without finding index"))
+}
+
 func main() {
 	b, err := ioutil.ReadFile("input")
 	if err != nil {
@@ -18,4 +35,5 @@ func main() {
 	}
 
 	fmt.Println("Answer 1:", FindLevel(string(b)))
+	fmt.Println("Answer 2:", FindLevelIndex(string(b), -1))
 }
